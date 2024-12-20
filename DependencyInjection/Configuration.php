3b4c8002fda2eb13191c14f2af7f788a087ed50a
IssabriCharafEddine <issabri.charafeddine@gmail.com>
2024-12-20 10:42:18 +0100

    change config of function getConfigTreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -16,7 +16,7 @@ class Configuration implements ConfigurationInterface
      *
      * @return TreeBuilder
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $tb = new TreeBuilder('sherlockode_advanced_content');
         $root = $tb->getRootNode();
